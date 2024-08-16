9e12c1df7157cdf9d669b47d584cb7db06f9bc14
HMErni <hero.misa@betterask.erni>
2024-08-16 13:12:16 +0800

    Added Expected Value

--- CalculatorApp.UnitTests/CalculatorTest.cs
+++ CalculatorApp.UnitTests/CalculatorTest.cs
@@ -19,48 +19,48 @@ namespace CalculatorApp.Tests
             _calculator = new Calculator(_logger);
         }
 
-        [TestCase(1, 2)]
-        public void Add_TwoNumbers_ShouldReturnSum(double num1, double num2)
+        [TestCase(65, 4, 69)]
+        public void Add_TwoNumbers_ShouldReturnSum(double num1, double num2, double expectedResult)
         {
             var unitUnderTest = _calculator.Add(num1, num2);
             unitUnderTest.ShouldBeOfType<Success<double>>();
 
             var successResult = unitUnderTest as Success<double>;
-            successResult?.value.ShouldBe(num1 + num2);
+            successResult?.value.ShouldBe(expectedResult);
 
         }
 
-        [TestCase(2, 1)]
-        public void Subtract_TwoNumbers_ShouldReturnDifference(double num1, double num2)
+        [TestCase(100, 31, 69)]
+        public void Subtract_TwoNumbers_ShouldReturnDifference(double num1, double num2, double expectedResult)
         {
             var unitUnderTest = _calculator.Subtract(num1, num2);
             unitUnderTest.ShouldBeOfType<Success<double>>();
 
             var successResult = unitUnderTest as Success<double>;
-            successResult?.value.ShouldBe(num1 - num2);
+            successResult?.value.ShouldBe(expectedResult);
         }
 
-        [TestCase(2, 1)]
-        public void Multiply_TwoNumbers_ShouldReturnProduct(double num1, double num2)
+        [TestCase(23, 3, 69)]
+        public void Multiply_TwoNumbers_ShouldReturnProduct(double num1, double num2, double expectedResult)
         {
             var unitUnderTest = _calculator.Multiply(num1, num2);
             unitUnderTest.ShouldBeOfType<Success<double>>();
 
             var successResult = unitUnderTest as Success<double>;
-            successResult?.value.ShouldBe(num1 * num2);
+            successResult?.value.ShouldBe(expectedResult);
         }
 
-        [TestCase(2, 4)]
-        public void Divide_TwoNumbers_ShouldReturnQuotient(double num1, double num2)
+        [TestCase(15, 3, 5)]
+        public void Divide_TwoNumbers_ShouldReturnQuotient(double num1, double num2, double expectedResult)
         {
             var unitUnderTest = _calculator.Divide(num1, num2);
             unitUnderTest.ShouldBeOfType<Success<double>>();
 
             var successResult = unitUnderTest as Success<double>;
-            successResult?.value.ShouldBe(num1 / num2);
+            successResult?.value.ShouldBe(expectedResult);
         }
 
-        [TestCase(6, 0)]
+        [TestCase(69, 0)]
         public void Divide_ByZero_ThrowsDivideByZeroException(double num1, double num2)
         {
             var unitUnderTest = _calculator.Divide(num1, num2);
@@ -72,7 +72,7 @@ namespace CalculatorApp.Tests
             failureResult?.exception.Message.ShouldBe("Cannot divide by zero.");
         }
 
-        [TestCase(6, 2, "modulus")]
+        [TestCase(69, 2, "modulus")]
         public void PerformOperation_InvalidOperation_ThrowsInvalidOperationException(double num1, double num2, string operation)
         {
             var unitUnderTest = _calculator.PerformOperation(num1, num2, operation);
